6e81407e04aac51b94505b4da4d940409b9d220f
sinithh <45849343+SinithH@users.noreply.github.com>
2026-06-02 23:32:43 +0530

    Service resolving

--- framework/src/Container/Container.php
+++ framework/src/Container/Container.php
@@ -2,25 +2,41 @@
 
 namespace Codex\Framework\Container;
 
-use Codex\Framework\Tests\DependantClass;
 use Psr\Container\ContainerInterface;
 
 class Container implements ContainerInterface
 {
     private array $services = [];
+
     public function add(string $id, string|object $concrete = null): void
     {
+        if (null === $concrete) {
+            $concrete = $id;
+
+            if (!class_exists($id)) {
+                throw new ContainerException("Service $id could not be found.");
+            }
+        }
+
         $this->services[$id] = $concrete;
     }
 
     public function get(string $id): object
     {
-          return new DependantClass();
-//        return new $this->services[$id]();
+        if (!$this->has($id)) {
+            if (class_exists($id)) {
+                throw new ContainerException("Service $id could not be resolved.");
+            }
+
+            $this->add($id);
+        }
+
+        $object = $this->resolve($this->services[$id]);
+        return $object;
     }
 
     public function has(string $id): bool
     {
-        return 0;// TODO: Implement has() method.
+        return array_key_exists($id, $this->services);
     }
 }
--- framework/tests/DependantClass.php
+++ framework/tests/DependantClass.php
@@ -4,5 +4,12 @@ namespace Codex\Framework\Tests;
 
 class DependantClass
 {
+    public function __construct(private DependancyClass $dependancy)
+    {
+    }
 
+    public function getDependency(): DependancyClass
+    {
+        return $this->dependancy;
+    }
 }
